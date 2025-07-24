fdd42816a8dcd7dd90544f1f2f3034aa96ff2e73
hungryakira <jon.kh@hotmail.com>
2025-07-24 15:19:47 +0800

    Finished version

--- src/Main.py
+++ src/Main.py
@@ -1,21 +1,5 @@
 # Typing speed is defined as "words" per minute. "word" is defined as 5 characters (including spaces, symbols)
-# Accuracy is % of correct words
-
-
-#TODO separate text into... 8 words / line?
-#   find out how many characters / line fits in the given text box size
-#   Parse text to separate characters into lines without words being out of bounds
-#   Finding out words/line is also create indexing for scrolling text
-
-#TODO Highlight one word at a time. Highlight next word after finished
-
-#TODO Typing changes existing text color. Red if incorrect.
-
-#TODO Can go back to previous character?
-
-#TODO After each line is completed, the line is pushed up, new one loaded
-
-#TODO Corrected CPM only counts correct characters. WPM is CPM / 5
+# Corrected CPM only counts correct characters. WPM is CPM / 5
 
 # Import Statements
 import json
@@ -24,107 +8,133 @@ from tkinter import ttk
 import tkinter as tk
 
 # Load text json
-# Word list found in Corpora - https://github.com/dariusk/corpora/tree/master
-with open('../data/common.json', 'r') as pw_file:
-    # reading old data
-    data = json.load(pw_file)
+# Word list from Corpora - https://github.com/dariusk/corpora/tree/master
 
 words = ""
+with open('../data/common.json', 'r') as pw_file:
+    data = json.load(pw_file)
 
-for n in range (0, 100):
+for n in range (0, 150):
     words += (data["commonWords"][randint(0, len(data["commonWords"]) - 1)]) + " "
 
+# Constants
+STATS_FONT = ('Calibri', '12')
+TEXT_FONT = ('Calibri', '16')
 
-# setup UI
 
 class TypeSpeedTest:
     def __init__ (self, root):
         self.root = root
-        self.setup_ui()
         self.characters_typed = 0
+        self.correct_char_count = 0
+        self.time_length = 60
+        self.timer = ""
 
+        self.setup_ui()
 
     def setup_ui(self):
+        """Function to set up UI"""
         main_frame = ttk.Frame(self.root, padding="10", )
         main_frame.grid(row = 0, sticky="nsew")
 
-        # for resizing
-        self.root.grid_rowconfigure(0, weight=1) #for resizing
-        self.root.grid_columnconfigure(0, weight=1)
-        main_frame.grid_rowconfigure(3, weight=1)
-
-        #Stats display
+        # Stats displays
         stats_frame = ttk.Frame(main_frame, padding="5")
         stats_frame.grid(row = 0)
-        stats_font = ('Calibri', '12')
 
-        #timer
-        time_label = ttk.Label(stats_frame,text = "time left", font = stats_font)
+        # Timer
+        time_label = ttk.Label(stats_frame, text = "Time", font = STATS_FONT)
         time_label.grid(padx = 5, row = 0, column =0)
-        time_counter = ttk.Label(stats_frame, text = "60", font = stats_font, background="white")
-        time_counter.grid(padx= 1, row = 0, column = 1)
+        self.time_counter = ttk.Label(stats_frame, text = self.time_length, font = STATS_FONT, background="white")
+        self.time_counter.grid(padx= 1, row = 0, column = 1)
 
-        #character count
-        chara_label = ttk.Label(stats_frame, text="Characters count", font=stats_font)
+        #Character count
+        chara_label = ttk.Label(stats_frame, text="Characters count", font=STATS_FONT)
         chara_label.grid(padx=5, row=0, column=2)
 
-        self.chara_count = ttk.Label(stats_frame, text="0", font=stats_font, background="white")
-        self.chara_count.grid(padx=5, row=0, column=3)
+        self.chara_count_label = ttk.Label(stats_frame, text="0", font=STATS_FONT, background="white")
+        self.chara_count_label.grid(padx=5, row=0, column=3)
 
-        # Text display https://tkdocs.com/tutorial/text.html
-        text_font = ('Calibri', '16')
+        #Correct character count
+        wpm_label = ttk.Label(stats_frame, text="Words per miunte", font=STATS_FONT)
+        wpm_label.grid(padx=5, row=0, column=4)
+
+        self.wpm_result = ttk.Label(stats_frame, text="?", font=STATS_FONT, background="white")
+        self.wpm_result.grid(padx=5, row=0, column=5)
+
         text_frame = ttk.Frame(main_frame)
         text_frame.grid(row = 1, sticky="nsew",padx = 5, pady = 5)
         self.textbox = tk.Text(text_frame,
-                          relief = "solid",
-                          font = text_font,
-                          width = 50,
-                          height = 3,
-                          wrap=tk.WORD) # Wrap full words
+                               relief = "solid",
+                               font = TEXT_FONT,
+                               width = 50,
+                               height = 5,
+                               wrap=tk.WORD) # Wrap full words
         self.textbox.insert("1.0", words)
         self.textbox.pack(fill="both", expand=True)
 
         # input box
         input_frame = ttk.Frame(main_frame)
         input_frame.grid(row=2, sticky="ew", padx=5, pady=5)
-        self.text_var = tk.StringVar(value="Type here!")
-        self.typing_box = ttk.Entry(input_frame, textvariable = self.text_var, font = text_font)
+        self.text_var = tk.StringVar(value="Click here to start test")
+        self.typing_box = ttk.Entry(input_frame, textvariable = self.text_var, font = TEXT_FONT)
         self.typing_box.pack(fill="x", expand = True)
-        self.text_var.trace_add("write", self.update_char_count)
+        self.text_var.trace_add("write", self.typing)
 
+    def typing(self, *args):
+        """Function to handle character count and scrolling the text box based on number of characters typed"""
 
-    def update_char_count(self, *args):
-        char_count = len(self.text_var.get())
-        self.chara_count.config(text=char_count)
+        current_entry = self.text_var.get()
+        self.characters_typed = len(current_entry)
+        self.chara_count_label.config(text=self.characters_typed)
+        # Keep roughly one line ahead current typing progress in Textbox
+        self.textbox.see(f"1.{self.characters_typed}+62 chars")
 
-    def clear_placeholder(self, event):
-        if self.typing_box.get() == "Type here!":
-            self.typing_box.delete(0, tk.END)
+    def match_text(self):
+        """Check Textbox and typing_box for matching blocks of texts and returns total number of matching characters."""
+        import difflib
 
-    def typing(self):
-        #count number of letters typed
-        #count each letter typed
-        #check if letter is correct per letter typed.
-        #TODO change color of letter typed
-        #TODO track character progress
-        #TODO track character correctness
-        pass
+        matcher = difflib.SequenceMatcher(None, words, self.typing_box.get(), autojunk=False)
+        match_results = matcher.get_matching_blocks()
+        # matcher returns a tuple: (text1 index, text2 index, length of match)
+        total_matches = 0
 
-    def next_word(self):
-        #TODO Scroll text
-        #TODO highlight word
-        #use "see" which takes an index.
-        #self.text.see(index)
-        pass
+        for i, j, n in match_results:
+            if n > 0:  # Skip the last row which is always (len(s1), len(s2), 0)
+                total_matches += n
 
+        #Calculate words per minute which is (correct characters/5)
+        word_per_min = int(total_matches/5)
+        self.wpm_result.config(text=word_per_min)
 
+    def start_counter(self,event):
+        """  Upon click on entry box, this function deletes default text and trigger count_down"""
+        global first_focus
+        if not first_focus: # only trigger when typing box clicked the first time
+            self.count_down(self.time_length)
 
+            if self.typing_box.get() == "Click here to start test":
+                self.typing_box.delete(0, tk.END)
+            first_focus = True
+
+    def count_down(self,count):
+        """countdown mechanism starts when textbox is clicked.
+        when timer finishes, trigger match_text, disable textbox"""
+
+        if count != 0:
+            self.time_counter.config(text=count)
+            self.timer = root.after(1000, self.count_down, count-1)
+        else:
+            root.after_cancel(self.timer)
+            self.typing_box.config(state="disabled")
+            self.time_counter.config(text="0")
+            self.match_text()
 
 
 if __name__ == "__main__":
     root=tk.Tk()
-    root.minsize(610, 200)
-    root.maxsize(610, 200)
+    root.minsize(585, 240)
+    root.maxsize(585, 240)
+    first_focus = False
     app=TypeSpeedTest(root)
-    app.typing_box.bind("<FocusIn>", app.clear_placeholder)
+    app.typing_box.bind("<FocusIn>", app.start_counter)
     root.mainloop()
